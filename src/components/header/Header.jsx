6a843bd02ad4c460d3dc4219d1e899009074e7e6
TANWIE12 <adeytanwie@gmail.com>
2023-05-15 17:52:28 +0100

    completetd tutorials  files

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import Me from '../../assets/me.jpg'
+import Headersocilas from './Headersocilas'
+
+function Header() {
+  return (
+    <header>
+      <div  className='container header__container'>
+      <h5>hello i'm</h5>
+      <h1>Tanwie Bruno</h1>
+      <h5 className='text-light'>Fullstack Developer</h5>
+      <CTA/>
+      <Headersocilas/>
+      <div className='me'>
+        <img src={Me} alt='my immage' ></img>
+      </div>
+      <a href='#contact'className='scroll__down'>scroll Down</a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
