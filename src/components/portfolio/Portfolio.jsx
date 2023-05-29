d188016b4e0c26ac73ce036a45710f23ea497a8b
TANWIE12 <adeytanwie@gmail.com>
2023-05-29 18:03:21 +0100

    added portfoilio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,91 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+
 function Portfolio() {
+  const data=[
+    
+      {
+    id:1,
+    Image:IMG1,
+    title:'BroadcastChannel',
+    github:'http://github.com/',
+    demo:'https://whasapp.com/'
+      },
+      {
+        id:2,
+        Image:IMG2,
+        title:'BroadcastChannel',
+        github:'http://github.com/',
+        demo:'https://whasapp.com/'
+          },
+          {
+            id:3,
+            Image:IMG3,
+            title:'BroadcastChannel',
+            github:'http://github.com/',
+            demo:'https://whasapp.com/'
+              },
+              {
+                id:4,
+                Image:IMG4,
+                title:'BroadcastChannel',
+                github:'http://github.com/',
+                demo:'https://whasapp.com/'
+                  }, 
+                  {
+                    id:5,
+                    Image:IMG5,
+                    title:'BroadcastChannel',
+                    github:'http://github.com/',
+                    demo:'https://whasapp.com/'
+                      },  
+           {
+                        id:6,
+                        Image:IMG6,
+                        title:'BroadcastChannel',
+                        github:'http://github.com/',
+                  demo:'https://whasapp.com/'
+                          },      
+
+  
+]
   return (
-    <section id='portfolio'>prot</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>portfolio</h2>
+      <div className="container portfolio__container">
+
+      {
+        data.map(({id,Image,title, github, demo})=>{
+          return(
+            <article  key={id} className='portfolio__item'>
+            <div className="portfolio__item-image">
+             <img src={Image} alt={title}/>
+            </div>
+            <h3>{title}</h3>
+            <div className="portfolio__item-cta">
+            <a href={github} className='btn'>Github</a>
+            <a href={demo} className='btn btn-primary' target='_blank' rel='noopener'>Livedemo</a>
+            </div>
+          </article>
+
+          )
+        })
+      }
+        
+
+       
+        
+        
+        </div> 
+    </section>
   )
 }
 
